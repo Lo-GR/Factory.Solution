db7ac275a25d88a6fe46116df053cf62be8e4dcf
Lo-GR <diamondintheroth@gmail.com>
2021-03-19 12:56:07 -0700

    create DeleteEngineer post route for machines

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -63,5 +63,13 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+    [HttpPost]
+    public ActionResult DeleteEngineer(int joinId)
+    {
+      EngineerMachine entry = _db.EngineerMachine.FirstOrDefault(join => join.EngineerMachineId == joinId);
+      _db.EngineerMachine.Remove(entry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
