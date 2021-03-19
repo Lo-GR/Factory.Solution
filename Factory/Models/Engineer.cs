7ad26b772d927d4900979a1088c067b27e17e91c
Lo-GR <diamondintheroth@gmail.com>
2021-03-19 09:41:08 -0700

    create Engineer Models

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -4,6 +4,12 @@ namespace Factory.Models
 {
   public class Engineer
   {
-    
+    public int EngineerId {get; set;}
+    public string EngineerName {get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities {get;}
+    public Engineer()
+    {
+      JoinEntities = new HashSet<EngineerMachine>();
+    }
   }
 }
